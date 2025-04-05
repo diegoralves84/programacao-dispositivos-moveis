ee9c57441518449559b62398711f2d17d31ec5d0
unknown <2324290066@ACADEMICO.IESB.BR>
2025-04-04 21:34:43 -0300

    Adicionando arquivos da pratica05

--- aulas/aula05/App.js
+++ aulas/aula05/App.js
@@ -1,13 +1,15 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import TaskScreen from "./screens/TaskScreen";
+import { TaskProvider } from "./contexts/TaskContext";
 
-
-function App () {
-return (
-  <SafeAreaProvider>
-    <TaskScreen />
-  </SafeAreaProvider>
-);
+function App() {
+  return (
+    <SafeAreaProvider>
+      <TaskProvider>
+        <TaskScreen />
+      </TaskProvider>
+    </SafeAreaProvider>
+  );
 }
 
 export default App;
