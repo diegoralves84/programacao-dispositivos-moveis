b4543683492ddfb68eedc320d284a0b6f19a6243
unknown <2324290066@ACADEMICO.IESB.BR>
2025-05-23 21:44:14 -0300

    Adicionando arquivos da aula08

--- aulas/aula08/components/ListaVertical.js
+++ aulas/aula08/components/ListaVertical.js
@@ -1,32 +1,24 @@
-import { View, FlatList } from 'react-native';
-import { List, Text } from 'react-native-paper';
+import { useContext } from "react";
+import { View, FlatList } from "react-native";
+import { List, Text } from "react-native-paper";
+
+import { LojaContext } from "../contexts/LojaContext";
 
 function ListaVertical() {
-    const lojas = [
-        "Restaurante Asa Sul",
-        "Restaurante Asa Norte",
-        "Restaurante SobraDisney",
-        "Bar CeiLondres",
-        "Butecos de PlanalCity",
-        "Bar Water Lakers",
-        "Pizzaria NuPelo"
-    ]
-    return (
-        <View>
-            <Text variant="titleMedium"> Lojas</Text>
-            <FlatList
-            data={lojas}
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(item, index) => index}
-            renderItem={({item})=> (
-                <List.Item
-                title={item}
-                description={"*4,8"}
-                />
-            )}
-            />
-        </View>
-    );
+  const { lojas } = useContext(LojaContext);
+  return (
+    <View>
+      <Text variant="titleMedium">Lojas</Text>
+      <FlatList
+        data={lojas}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => (
+          <List.Item title={item.nome} description={`* ${item.nota}`} />
+        )}
+      />
+    </View>
+  );
 }
 
 export default ListaVertical;
